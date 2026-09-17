b0978426eedf3c23332920339c3fea53284165d2
OrdaxVictor <victor.leon@ordaxemplea.com>
2026-09-17 17:43:08 +0200

    fixed string names, translations and added action button to visit

--- real_estate_victor/models/realestate_property.py
+++ real_estate_victor/models/realestate_property.py
@@ -6,7 +6,7 @@ class RealEstateProperty(models.Model):
     _name = 'realestate.property'
     _description = 'RealEstateProperty'
 
-    name = fields.Char(string='Propiedad')
-    desc = fields.Char(string='Descripción')
-    size = fields.Float(string='Metros cuadrados')
-    user_id = fields.Many2one(comodel_name="res.users", string="Responsable")
+    name = fields.Char(string='Property')
+    desc = fields.Char(string='Description')
+    size = fields.Float(string='Size')
+    user_id = fields.Many2one(comodel_name="res.users", string="Manager")
