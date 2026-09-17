1845be63200d19ece1eee1d6a8e7720a534eb583
OrdaxVictor <victor.leon@ordaxemplea.com>
2026-09-17 17:47:17 +0200

    fixed string names, translations and added action button to visit

--- real_estate_victor/models/realestate_visit.py
+++ real_estate_victor/models/realestate_visit.py
@@ -10,7 +10,7 @@ class RealEstateVisit(models.Model):
     date = fields.Datetime(string='Date')
     property = fields.Many2one(comodel_name='realestate.property', string="Property")
     contact = fields.Many2one(comodel_name='res.partner', string='Contact')
-    status = fields.Selection([('P', 'Pending'), ('V', 'Visited', ('R', 'Reserved'))],string='Status', default='P',)
+    status = fields.Selection([('P', 'Pending'), ('V', 'Visited'), ('R', 'Reserved'))],string='Status', default='P',)
 
     def action_reserve(self):
         self.write({'status': 'R'})
