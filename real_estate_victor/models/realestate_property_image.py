6a6350d7527289ed569ccffe71ac41d15f035359
OrdaxVictor <victor.leon@ordaxemplea.com>
2026-09-23 18:13:50 +0200

    task one2many and more

--- real_estate_victor/models/realestate_property_image.py
+++ real_estate_victor/models/realestate_property_image.py
@@ -4,9 +4,6 @@ class RealestatePropertyImage(models.Model):
     _name = "realestate.property.image"
     _description = "Property Image"
 
-    def _default_name(self):
-        return "Imagen "
-
     name = fields.Char(string="Name", default=_default_name())
     sequence = fields.Integer(string="Sequence", default=10)
     property_id = fields.Many2one(
@@ -15,3 +12,6 @@ class RealestatePropertyImage(models.Model):
         ondelete="cascade",
     )
     image = fields.Binary(string="Image")
+
+    def _default_name(self):
+        return "Imagen "
--- real_estate_victor/models/realestate_property_incidence.py
+++ real_estate_victor/models/realestate_property_incidence.py
@@ -4,9 +4,6 @@ class RealestatePropertyIncidence(models.Model):
     _name = "realestate.property.incedence"
     _description = "Property Incidence"
 
-    def _default_name(self):
-        return "Incidencia "
-
     name = fields.Char(string="Name", default=_default_name())
     sequence = fields.Integer(string="Sequence", default=10)
     property_id = fields.Many2one(
@@ -19,3 +16,6 @@ class RealestatePropertyIncidence(models.Model):
     status = fields.Selection([('P', 'Pending'),('S','Solved')], string='Status', default='P')
     date = fields.Datetime(string='Date')
     user_id = fields.Many2one(comodel_name='res.users', string='Manager')
+
+    def _default_name(self):
+        return "Incidencia "
