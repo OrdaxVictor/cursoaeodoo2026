07819b89497947045f394181c2a36f5df102b97f
OrdaxVictor <victor.leon@ordaxemplea.com>
2026-09-24 16:50:28 +0200

    task 7 - offer updates and more

--- real_estate_victor/models/realestate_property.py
+++ real_estate_victor/models/realestate_property.py
@@ -59,7 +59,7 @@ class RealEstateProperty(models.Model):
     def _compute_next_visit_date(self):
         for record in self:
             dates = record.visit_ids
-            record.next_visit_date = min(dates).name
+            record.next_visit_date = min(dates).date
 
     def action_reserve(self):
         self.availability = False
